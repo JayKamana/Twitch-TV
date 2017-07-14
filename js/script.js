1af770f7b2a781bad4881d183525ff5aa8123251
James Kamana <jaykamana@gmail.com>
2017-07-14 22:43:47 +0300

    add profile images and twitch tv link

--- js/script.js
+++ js/script.js
@@ -2,11 +2,8 @@ $(function(){
 
 	var streamers = ["lirik", "freecodecamp", "pokket", "kindafunnygames", "DansGaming"]
 
-	$('.twitch-btn').on('click', function(){
-		checkStream();
-	})
+	function checkStream(){
 
-	var checkStream = function(){
 		streamers.forEach(function(streamer){
 
 			fetch('https://api.twitch.tv/kraken/streams/'+streamer+'?client_id=4told1vdrf3s0axfrd86lig9c4dawc')
@@ -16,8 +13,9 @@ $(function(){
 
 
 				if (data.stream != null){
-					$('.'+streamer+' + .live').append('Online');
-					console.log(data.stream.game)
+					
+					$('.'+streamer).html('<img class="img-responsive img-circle stream-logo" src="'+data.stream.channel.logo+'" /><a href="'+data.stream.channel.url+'">'+streamer+'</a>');
+					$('.'+streamer+' + .live').html('Online');
 					$('.'+streamer+' ~ .game').html(data.stream.game)
 
 				}
@@ -30,7 +28,7 @@ $(function(){
 			})
 
 		})
-	}
+	};
 
 
 	function checkStatus (response){
@@ -45,6 +43,10 @@ $(function(){
 	function getJSON (response){
 		return response.json();
 	}
+
+	$('.twitch-btn').on('click', function(){
+		checkStream();
+	})
 
 	checkStream();
  })
